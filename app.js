022f0fddd316295ada9af95f62148f2cc7420d76
水戸 匠 <250158@yse.ac.jp>
2026-01-28 11:28:09 +0900

    コメント・JSdocの追加

--- app.js
+++ app.js
@@ -13,13 +13,30 @@ let score1 = 0;
 let score2 = 0;
 let gamePoint = 1;
 
+/**
+ * ボタン操作イベントが発火すると呼び出される。
+ * 点数更新・ゲーム進行状況の取得・ゲーム終了時の画面更新を担う
+ */
 function mainProcess () {
     updateScoreAndSheet(this);
     const nowStatement = checkStatement(score1, score2, gamePoint);
     console.log(nowStatement)
     // TODO：勝敗が確定したらCSSの変更と得点追加ボタンのロックを実行する。
+    switch (nowStatement) { 
+        case WIN_PLAYER_1:
+            break;
+        case WIN_PLAYER_2:
+            break;
+        default:
+            // ゲーム続行
+            break;
+    }
 }
 
+/**
+ * ボタン操作に応じて画面の点数を更新
+ * @param {Element} button - player1, player2, resetのいずれかのHTML要素 
+ */
 function updateScoreAndSheet (button) {
     switch (button) {
         case button1:
@@ -39,6 +56,13 @@ function updateScoreAndSheet (button) {
     }
 }
 
+/**
+ * ゲーム進行状態を調査する。点数が加算される度に実行される。
+ * @param {*} score1 - player1の現在の点数
+ * @param {*} score2 - player2の現在の点数
+ * @param {*} gamePoint 
+ * @returns {String} 現在のゲーム進行状態
+ */
 function checkStatement (score1, score2, gamePoint) {
     if (score1 === gamePoint) {
         return WIN_PLAYER_1;
@@ -49,10 +73,12 @@ function checkStatement (score1, score2, gamePoint) {
     }
 }
 
+// ボタン操作監視
 button1.addEventListener('click', mainProcess)
 button2.addEventListener('click', mainProcess);
 resetButton.addEventListener('click', mainProcess);
 
+// セレクトオプション操作監視
 maxPoint.addEventListener('change',  function () {
     gamePoint = parseInt(maxPoint.value);
 });
